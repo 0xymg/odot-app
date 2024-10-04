7f87581bfedd7751dd06af3be94d54d02c045b4d
Yunus Melih Gozutok <ymgozutok@gmail.com>
2024-10-05 01:13:43 +0200

    remove before launch

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,15 @@
+import { Content, RootLayout, Sidebar, DraggableTopBar } from './components'
+
+const App = (): JSX.Element => {
+  return (
+    <>
+      <DraggableTopBar />
+      <RootLayout>
+        <Sidebar className="p-2">Sidebar</Sidebar>
+        <Content className="border-l bg-zinc-900/50 border-l-white/20">Content</Content>
+      </RootLayout>
+    </>
+  )
+}
+
+export default App
